0ec98b6247574b70bebfa0d567d96c37cc105793
lBurstLimitl <132276560+lBurstLimitl@users.noreply.github.com>
2025-06-18 22:15:04 +0800

    Update player.rs

--- src/player.rs
+++ src/player.rs
@@ -5,7 +5,7 @@ pub fn play_from_url(url: &str) -> Result<(), Box<dyn std::error::Error>> {
     let (_stream, handle) = OutputStream::try_default()?;
     let sink = Sink::try_new(&handle)?;
 
-    let resp = ureq::get(url).call().into_reader();
+    let resp = ureq::get(url).call()?.into_reader();
     let source = Decoder::new(BufReader::new(resp))?;
     sink.append(source);
     sink.sleep_until_end();
